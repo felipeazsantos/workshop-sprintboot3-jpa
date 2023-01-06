7a67590baf988bd9c38a65c652a0bae947b51981
Felipe Azevedo <felipeasantos10@gmail.com>
2023-01-06 19:47:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/felipeazsantos/springbootcourse/services/UserService.java
@@ -0,0 +1,26 @@
+package com.felipeazsantos.springbootcourse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipeazsantos.springbootcourse.entities.User;
+import com.felipeazsantos.springbootcourse.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
