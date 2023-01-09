3b3ae0596eedf055ce74e747352742dd3eeec8c0
Felipe Azevedo <felipeasantos10@gmail.com>
2023-01-09 19:02:25 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/felipeazsantos/springbootcourse/entities/Order.java
+++ src/main/java/com/felipeazsantos/springbootcourse/entities/Order.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.felipeazsantos.springbootcourse.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
